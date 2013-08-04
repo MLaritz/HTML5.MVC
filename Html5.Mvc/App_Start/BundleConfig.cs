4f6f91574c0f06fd79dbddf53c421c0fe997a05a
Desk Coder <dc@deskcoder.com>
2013-08-03 21:48:03 -0400

    Initial version

---
+++ Html5.Mvc/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Optimization;
+
+namespace Html5.Mvc
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //Scripts
+
+            bundles.Add(new ScriptBundle("~/bundles/html5shiv")
+                .Include("~/Scripts/html5shiv.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/html5shiv-printshiv")
+                .Include("~/Scripts/html5shiv-printshiv.js"));
+
+            //Styles
+
+            bundles.Add(new StyleBundle("~/content/css")
+                            .Include("~/Content/normailize.css",
+                                     "~/Content/site.css"));
+        }
+    }
+}
---
+++ Html5.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Html5.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
